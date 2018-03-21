07db23324b73c25fd421a72e33f7181ebe52b670
seeng1309 <seeng1309@7LLN6Y1.Student.UGDSB.ED>
2018-03-21 11:12:47 -0400

    Carpet

---
+++ src/carpetmydudes/CarpetMyDudes.java
@@ -0,0 +1,27 @@
+/*
+ * Seth James England
+ * February 27, 2018
+ */
+
+package carpetmydudes;
+
+/**
+ * Seth England
+ */
+public class CarpetMyDudes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double length = 8.5;
+        double width = 6;
+        double area = length * width;
+        double price = 19.95;
+        System.out.println("Ya carpet costs " + area * price + "$ holy frick thats a lot of money");
+        
+        
+        
+    }
+    
+}
